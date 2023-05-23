2ae09bdcb530c0fe17fd508c9b12731522a9624d
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2023-05-23 10:00:41 -0700

    First files.

---
+++ src/Resources.js
@@ -0,0 +1,18 @@
+export {Resources};
+
+
+class Resources {
+
+
+    constructor() {
+        // Fetch the JSON file.
+    }
+
+
+    // Return the JSX component.
+    render() {
+
+    
+    }
+
+}
